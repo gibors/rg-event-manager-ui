0d493ced1aec435ccaa58bceb02140efc6363475
GibrannReyesTR <Gibrann.Reyes@thomsonreuters.com>
2025-06-02 13:45:46 -0600

    add  generate nomina button

--- rgeventmanagerui/lib/modules/operations_general/general_nominal.dart
+++ rgeventmanagerui/lib/modules/operations_general/general_nominal.dart
@@ -25,13 +25,35 @@ class _GeneralNominalPage extends State<GeneralNominalPage> {
             'Sistema RG Eventos - Usuario conectado: ${appState.selectedUser != null ? '${appState.selectedUser!.name} ${appState.selectedUser!.lastname} con rol de ${appState.selectedUser!.role == 1 ? 'admin' : (appState.selectedUser!.role == 2 ? 'operativo' : 'solo lectura')}' : ''}',
             style: TextStyle(
                 fontSize: 28.0, color: const Color.fromARGB(255, 113, 7, 132))),
- 
-
-
       ),
-      body: Center(
-        child: Text('General Nominal Page'),
-      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 22,
+            ),
+            Row(
+              children: [
+                TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.grey,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/general');
+                  },
+                  child: Text(
+                    'Generar Nomina',
+                    style: TextStyle(fontSize: 20.0, color: Colors.black),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        )
+      ,)
     );
   }  
 }
